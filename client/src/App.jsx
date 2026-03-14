2047be59fe768a087524aa1daf27bde2c84df5c8
Adii <itsaditya101@gmail.com>
2026-03-14 22:35:27 +0530

    authentication using firebase done

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/auth' element={<Auth />} />
+      </Routes>
+
+    </>
+  )
+}
+
+export default App
